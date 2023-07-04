566f062aab959786cff4b1841e40c368253b72f0
Glend <35920161+glendless@users.noreply.github.com>
2023-07-04 21:51:09 +0700

    Update astro.config.mjs
    delete output: server

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,6 @@ import config from "./src/config/config.json";
 // https://astro.build/config
 export default defineConfig({
   // site: config.site.base_url ? config.site.base_url : "http://examplesite.com",
-  output: "server",
   site: config.site.base_url ? config.site.base_url : "http://astroplate.netlify.app",
   base: config.site.base_path ? config.site.base_path : "/",
   trailingSlash: config.site.trailing_slash ? "always" : "never",
